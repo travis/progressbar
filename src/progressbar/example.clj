261700002710d13cb9418d15bc0a537817380ba1
Travis Vachon <travis@copious.com>
2013-08-08 11:08:18 -0400

    Add another example for manual smoke tests

--- src/progressbar/example.clj
+++ src/progressbar/example.clj
@@ -2,8 +2,10 @@
   (:require [progressbar.core :refer [progressbar]]))
 
 (defn example1 []
-  (map #(do (Thread/sleep 100) %) (progressbar (vec (range 100)))))
+  (dorun (map #(do (Thread/sleep 100) %) (progressbar (range 100)))))
 
 (defn example2 []
-  (doall (map #(do (Thread/sleep 100) %) (progressbar (range 101) :count 101 :width 80)))
-  nil)
+  (dorun (map #(do (Thread/sleep 100) %) (progressbar (range 101) :count 101 :width 80))))
+
+(defn example3 []
+  (dorun (map #(do (Thread/sleep 100) %) (progressbar (vec (range 100))))))
