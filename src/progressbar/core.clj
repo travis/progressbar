195a08cad2a8988fd55f39ae9de4135afca2a259
Travis Vachon <travis@copious.com>
2013-08-07 22:16:03 -0400

    Factor progress seq functions into their own namespace

--- src/progressbar/core.clj
+++ src/progressbar/core.clj
@@ -1,40 +1,5 @@
 (ns progressbar.core
-  (:require [clojure.string :as str]))
-
-
-(defn unbounded-progress-seq [print-every]
-  (map (fn [index]
-         (when (= 0 (mod index print-every))
-           (str "\r["(str/join (take (int (/ index print-every)) (repeat "=")))")")))
-       (iterate inc 0)))
-
-(defn bounded-progress-seq [cnt & {:keys [width]
-                                   :or {width 64}}]
-  {:pre [(number? width) (pos? width)] }
-  "Produces a seq like:
-
- [         ]
- [=        ]
- [==       ]
- [===      ]
- [====     ]
- [=====    ]
- ...
- [=========]
-"
-
-  (let [items-per-equals (int (/ cnt width))]
-   (map (fn [index]
-          (let [n (inc index) ; index starting from 1
-                last-n-remainder (mod cnt items-per-equals)
-                segment-processed? #(= (mod n items-per-equals) last-n-remainder)]
-            (when (segment-processed?)
-              (let [number-of-equals (int (* width (/ n cnt)))]
-                (apply str (concat ["\r["]
-                                   (take number-of-equals (repeat "="))
-                                   (take (- width number-of-equals) (repeat " "))
-                                   ["]"]))))))
-        (iterate inc 0))))
+  (:require [progressbar.progress-seq :as progress-seq]))
 
 (defn progressbar
   "Transparently wrap any `seq`able to print feedback to standard out as items in
@@ -47,30 +12,11 @@ the seq are processed.
           :or {print-every 10
                width 64}}]
   (let [progress-seq (or progress-seq (if count
-                                        (bounded-progress-seq count :width width)
-                                        (unbounded-progress-seq print-every)))]
+                                        (progress-seq/bounded count :width width)
+                                        (progress-seq/unbounded print-every)))]
     (map (fn [item progress]
            (when progress
              (print progress)
              (flush))
            item)
          seq progress-seq)))
-
-(comment
-
-
-
-  (condp = 3
-      1 "insane"
-      2 "less insane"
-      3 "CRAZY")
-
-  (let [total-number-of-equals 10
-        count 100
-        index 99
-
-        ]
-    )
-
-
-  )
---
+++ src/progressbar/progress_seq.clj
@@ -0,0 +1,38 @@
+(ns progressbar.progress-seq
+  (:require [clojure.string :as str]))
+
+(defn unbounded [print-every]
+  (map (fn [index]
+         (when (= 0 (mod index print-every))
+           (str "\r["(str/join (take (int (/ index print-every)) (repeat "=")))")")))
+       (iterate inc 0)))
+
+(defn bounded [cnt & {:keys [width]
+                                   :or {width 64}}]
+  {:pre [(number? width) (pos? width)] }
+  "Produces a seq like:
+
+ [         ]
+ [=        ]
+ [==       ]
+ [===      ]
+ [====     ]
+ [=====    ]
+ ...
+ [=========]
+"
+
+  (let [items-per-equals (int (/ cnt width))]
+   (map (fn [index]
+          (let [n (inc index) ; index starting from 1
+                last-n-remainder (mod cnt items-per-equals)
+                segment-processed? #(= (mod n items-per-equals) last-n-remainder)]
+            (when (segment-processed?)
+              (let [number-of-equals (int (* width (/ n cnt)))]
+                (apply str (concat ["\r["]
+                                   (take number-of-equals (repeat "="))
+                                   (take (- width number-of-equals) (repeat " "))
+                                   ["]"]))))))
+        (iterate inc 0))))
+
+
